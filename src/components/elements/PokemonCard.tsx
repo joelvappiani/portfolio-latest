d508d52c78a7377a86df522dbc57aa72e88987e9
djovap <joel.vappiani@gmail.com>
2023-10-17 17:21:18 +0200

    finished pokemon card

--- src/components/elements/PokemonCard.tsx
+++ src/components/elements/PokemonCard.tsx
@@ -1,63 +1,116 @@
 "use client"
-import React, { useEffect, useState, useRef } from 'react'
-import style from '../../styles/card.module.css'
+import React, { useState, useCallback, MouseEvent } from 'react'
+
 
 
 const TechLogo = () => {
     const [hovered, setHovered] = useState<boolean>(false)
-    const [dimentions, setDimentions] = useState({ width: 0, height: 0 })
-    const [cursorX, setCursorX] = useState(0)
-    const [cursorY, setCursorY] = useState(0)
+    const [rotate, setRotate] = useState({ x: 0, y: 0 });
 
-    const ref = useRef<HTMLDivElement>(null)
-
-    useEffect(() => {
-        console.log(hovered)
-
-        ref.current && setDimentions({ width: ref.current?.clientWidth, height: ref.current?.clientHeight })
-        const moveCursor = (e: any) => {
-            if (hovered) {
-                setCursorX(e.clientX)
-                setCursorY(e.clientY)
+    function throttle<T extends (...args: any[]) => any>(
+        func: T,
+        delay: number
+    ): (...args: Parameters<T>) => void {
+        let lastCall = 0;
+        return (...args: Parameters<T>) => {
+            const now = new Date().getTime();
+            if (now - lastCall < delay) {
+                return;
             }
+            lastCall = now;
+            return func(...args);
         };
-        window.addEventListener('mousemove', moveCursor)
-        return () => {
-            window.removeEventListener('mousemove', moveCursor)
-        }
-    }, [hovered])
-
-    function handleMouseLeave() {
-
-        setCursorX(0)
-        setCursorY(0)
     }
+    const onMouseMove = useCallback(
+        throttle((e: MouseEvent<HTMLDivElement>) => {
+            setHovered(true)
+            const card = e.currentTarget;
+            card.classList.add('active')
+            const box = card.getBoundingClientRect();
+            const x = e.clientX - box.left;
+            const y = e.clientY - box.top;
+            const centerX = box.width / 2;
+            const centerY = box.height / 2;
+            const rotateX = (y - centerY) / 7;
+            const rotateY = (centerX - x) / 7;
+            setRotate({ x: rotateX, y: rotateY });
+        }, 100),
+        []
+    );
+
+    const onMouseLeave = () => {
+        setHovered(false)
+        setRotate({ x: 0, y: 0 });
+    };
 
 
-    const positionX = Math.abs(Math.floor(100 / dimentions.width * Number(cursorX)) - 100);
-    const positionY = Math.abs(Math.floor(100 / dimentions.height * Number(cursorY)) - 100);
-    console.log(positionX, positionY)
-    //math for gradient
-    const pa = (50 - positionX) + (50 - positionY);
-    const lp = (50 + (positionX - 50) / 1.5);
-    const tp = (50 + (positionY - 50) / 1.5);
-    const pxSpark = (50 + (positionX - 50) / 7);
-    const pySpark = (50 + (positionY - 50) / 7);
-    const pOpacity = 20 + (Math.abs(pa) * 1.5);
-    const ty = ((tp - 50) / 2) * -1;
-    const tx = ((lp - 50) / 1.5);
 
     return (
         <div className='relative w-[21em] h-[30em]'>
-            <div className={`card-after ${hovered && "active"} absolute top-0 left-0 bottom-0 right-0 pointer-events-none`} style={{ backgroundPosition: `${pxSpark}% ${pySpark}%`, opacity: `${pOpacity / 100}`, transform: `rotateX(${ty}deg) rotateY(${tx}deg)` }} />
             <div
-                ref={ref}
-                onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => handleMouseLeave()}
-                className={`card ${hovered ? "active" : ''} ${style.card} absolute top-0 left-0 right-0 bottom-0 w-[21em] h-[30em]`}
-                style={{ backgroundImage: `url('https://assets.codepen.io/13471/charizard-gx.webp')`, transform: `rotateX(${ty}deg) rotateY(${tx}deg)` }}
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    pointerEvents: 'none',
+                    backgroundImage: "url(https://assets.codepen.io/13471/sparkles.gif),url(https://assets.codepen.io/13471/holo.png),linear-gradient(125deg, #ff008450 15%, #fca40040 30%, #ffff0030 40%, #00ff8a20 60%, #00cfff40 70%, #cc4cfa50 85%)",
+                    backgroundSize: "160%",
+                    backgroundPosition: `${(50 + (rotate.x - 50) / 1.5)}% ${(50 + (rotate.y - 50) / 1.5)}%`,
+                    backgroundBlendMode: "overlay",
+                    zIndex: 4,
+                    filter: !hovered ? "brightness(O.5) contrast(1)" : "brightness(1) contrast(1)",
+                    mixBlendMode: "color-dodge",
+                    borderRadius: '5% / 3.5%',
+                    transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${rotate.y}deg) scale3d(1, 1, 1)`,
+                    transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s"
+                }} />
+            <div
+                onMouseMove={onMouseMove}
+                onMouseLeave={onMouseLeave}
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    overflow: "hidden",
+                    zIndex: 1,
+                    borderRadius: "5% / 3.5%",
+                    willChange: "transform, filter",
+                    backgroundSize: "110%",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "50% 50%",
+                    transformOrigin: "center",
+                    width: "21em",
+                    height: "30em",
+                    backgroundImage: "url('https://assets.codepen.io/13471/charizard-gx.webp')",
+                    transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${rotate.y}deg) scale3d(1, 1, 1)`,
+                    transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+                    boxShadow: '-20px -20px 30px -25px var(--color1),20px 20px 30px -25px var(--color2),-7px -7px 10px -5px var(--color1),7px 7px 10px -5px var(--color2),0 0 13px 4px rgba(255, 255, 255, 0.3),0 55px 35px -20px rgba(0, 0, 0, 0.5)',
+
+                }}
             />
-            <div className={`card-before ${hovered ? "active" : "opacity-0"} absolute top-0 left-0 right-0 bottom-0 pointer-events-none w-[21em] h-[30em]`} style={{ backgroundPosition: `${lp}% ${tp}%`, transform: `rotateX(${ty}deg) rotateY(${tx}deg)` }} />
+            <div
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    pointerEvents: "none",
+                    borderRadius: "5% / 3.5%",
+                    backgroundSize: "300% 100%",
+                    backgroundImage: "linear-gradient(115deg,transparent 0%,#fac 25%,transparent 47%,transparent 53%,#ddccaa 75%,transparent 100%)",
+                    opacity: 1,
+                    backgroundPosition: `${((120 - rotate.x) / 1.5)}% ${(50 + (rotate.y) / 1.5)}%`,
+                    filter: "brightness(.5) contrast(1)",
+                    zIndex: 2,
+                    transform: `perspective(1000px) rotateX(${rotate.x}deg) rotateY(${rotate.y}deg) scale3d(1, 1, 1)`,
+                    transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+                    backgroundRepeat: "no-repeat",
+                    mixBlendMode: "color-dodge",
+                }}
+            />
 
         </div>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,8 @@ const config: Config = {
         'red': '#fb4303',
         'black': 'black',
         'white': 'white',
+        'pokemonPink': "#fac",
+        "pokemonYellow": "#ddccaa",
         transparent: 'transparent',
         current: 'currentColor',
       },
@@ -39,7 +41,8 @@ const config: Config = {
         'neumorphism-button-orange': 'inset -5px -5px 6px #b76f2e,inset 5px 5px 6px #ffb54a, 7px 7px 14px #cfd1d1,-7px -7px 14px #ffffff',
         'neumorphism-button-purple': 'inset -5px -5px 6px #ff35a3,inset 5px 5px 6px #b33eff, 7px 7px 14px #b3b3b3,-7px -7px 14px #ffffff',
         'neumorphism-button-white': "inset -8px -8px 9px #bababa,inset 8px 8px 9px #ffffff",
-        'neumorphism-image': "inset -7px -7px 14px #cfd1d1,inset 7px 7px 14px #ffffff, 7px 7px 14px #cfd1d1,-7px -7px 14px #ffffff"
+        'neumorphism-image': "inset -7px -7px 14px #cfd1d1,inset 7px 7px 14px #ffffff, 7px 7px 14px #cfd1d1,-7px -7px 14px #ffffff",
+        'pokemon-card': 'box-shadow:-5px -5px 5px -5px #fac,5px 5px 5px -5px #ddccaa,-7px -7px 10px -5px transparent,7px 7px 10px -5px transparent,0 0 5px 0px rgba(255, 255, 255, 0),0 55px 35px -20px rgba(0, 0, 0, 0.5)'
       },
       borderRadius: {
         '4xl': "30px"
