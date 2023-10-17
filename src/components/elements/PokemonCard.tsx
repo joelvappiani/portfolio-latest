858189d7baf757563c26646476d83be138193394
djovap <joel.vappiani@gmail.com>
2023-10-17 12:43:13 +0200

    backup for working pokemon card

--- src/components/elements/PokemonCard.tsx
+++ src/components/elements/PokemonCard.tsx
@@ -38,26 +38,26 @@ const TechLogo = () => {
     const positionY = Math.abs(Math.floor(100 / dimentions.height * Number(cursorY)) - 100);
     console.log(positionX, positionY)
     //math for gradient
-    const pa = (100 - positionX) + (100 - positionY);
-    const lp = (100 + (positionX - 100) / 1.5);
-    const tp = (100 + (positionY - 100) / 1.5);
-    const pxSpark = (100 + (positionX - 100) / 7);
-    const pySpark = (100 + (positionY - 100) / 7);
+    const pa = (50 - positionX) + (50 - positionY);
+    const lp = (50 + (positionX - 50) / 1.5);
+    const tp = (50 + (positionY - 50) / 1.5);
+    const pxSpark = (50 + (positionX - 50) / 7);
+    const pySpark = (50 + (positionY - 50) / 7);
     const pOpacity = 20 + (Math.abs(pa) * 1.5);
-    const ty = ((tp - 100) / 2) * -1;
-    const tx = ((lp - 100) / 1.5) * .5;
+    const ty = ((tp - 50) / 2) * -1;
+    const tx = ((lp - 50) / 1.5);
 
     return (
-        <div className='relative top-20 left-96 w-[21em] h-[30em]'>
-            <div className={`card-after ${hovered && "active"} absolute top-0 left-0 righ-0 bottom-0 h-full w-full pointer-events-none`} style={{ backgroundPosition: `${pxSpark}% ${pySpark}%`, opacity: `${pOpacity / 100}`, transform: `rotateX(${ty}deg) rotateY(${tx}deg)` }} />
+        <div className='relative w-[21em] h-[30em]'>
+            <div className={`card-after ${hovered && "active"} absolute top-0 left-0 bottom-0 right-0 pointer-events-none`} style={{ backgroundPosition: `${pxSpark}% ${pySpark}%`, opacity: `${pOpacity / 100}`, transform: `rotateX(${ty}deg) rotateY(${tx}deg)` }} />
             <div
                 ref={ref}
                 onMouseEnter={() => setHovered(true)}
                 onMouseLeave={() => handleMouseLeave()}
-                className={`card ${hovered && "active"} ${style.card} absolute top-0 left-0 righ-0 bottom-0  h-full w-full`}
+                className={`card ${hovered ? "active" : ''} ${style.card} absolute top-0 left-0 right-0 bottom-0 w-[21em] h-[30em]`}
                 style={{ backgroundImage: `url('https://assets.codepen.io/13471/charizard-gx.webp')`, transform: `rotateX(${ty}deg) rotateY(${tx}deg)` }}
             />
-            <div className={`card-before ${hovered ? "active" : "opacity-0"} absolute top-0 left-0 righ-0 bottom-0 pointer-events-none  h-full w-full`} style={{ backgroundPosition: `${lp}% ${tp}%`, transform: `rotateX(${ty}deg) rotateY(${tx}deg)` }} />
+            <div className={`card-before ${hovered ? "active" : "opacity-0"} absolute top-0 left-0 right-0 bottom-0 pointer-events-none w-[21em] h-[30em]`} style={{ backgroundPosition: `${lp}% ${tp}%`, transform: `rotateX(${ty}deg) rotateY(${tx}deg)` }} />
 
         </div>
     )
