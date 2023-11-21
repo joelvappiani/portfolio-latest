076991f02153539cf7aae8a7b3433020e3fa6faa
joelvappiani <joel.vappiani@gmail.com>
2023-11-21 16:41:03 +0100

    image contact

--- src/components/elements/contact/ContactImage.tsx
+++ src/components/elements/contact/ContactImage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useAnimate, useInView } from 'framer-motion'
+import Image from 'next/image'
 const ContactImage = () => {
     const [scope, animate] = useAnimate()
     const isInView = useInView(scope)
@@ -21,7 +22,7 @@ const ContactImage = () => {
             className='hidden relative rounded-xl top-20 lg:left-[10%] xl:left-[15%] 2xl:left-[20%] opacity-0 lg:flex justify-center w-[80vw] h-60'
         >
             <div className='absolute top-0 left-0 m-0 w-[390px] h-[410px] bg-white rounded-xl'></div>
-            <img src='/images/avatar/paper-plane.png' width={370} height={390} className=' image absolute top-0 left-[10px] w-[370px] h-[390px] m-0 rounded-xl saturate-150' alt='paper-plane-img' />
+            <Image src='/images/avatar/paper-plane.png' width={370} height={390} className=' image absolute top-0 left-[10px] w-[370px] h-[390px] m-0 rounded-xl saturate-150' alt='paper-plane-img' />
             <div className='absolute top-0 left-0  w-[390px] h-[410px] opacity-10 bg-gray-500 shadow-neumorphism-image rounded-xl m-0'></div>
             <div className='absolute top-0 left-0  w-[390px] h-[410px]  shadow-neumorphism-image rounded-xl m-0'></div>
         </div>
