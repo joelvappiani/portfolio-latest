e2b2f1b51bb2c632ef12ad3570e572e8a1a275e4
joelvappiani <joel.vappiani@gmail.com>
2023-12-14 16:14:59 +0100

    target blank on link

--- src/components/elements/BrandLogo.tsx
+++ src/components/elements/BrandLogo.tsx
@@ -30,14 +30,14 @@ const BrandLogo = ({ src, desc, link }: BrandLogoProps): JSX.Element => {
     return (
 
 
-        <a href={link} ref={scope} className='tech-logo flex flex-col items-center w-20 z-30 cursor-pointer' onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} >
+        <a href={link} ref={scope} target='_blank' className='tech-logo flex flex-col items-center w-20 z-30 cursor-pointer' onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} >
 
             <Image src={`/images/icons/icon-3d-${src}.png`} width={100} height={100} className={`w-[40px] h-[40px]`} alt={`${src} logo`} />
 
             <span className='opacity-0 description text-transparent bg-clip-text bg-gradient-to-r from-gray-900 to-gray-200 text-center text-xs w-[200px] '>{desc}</span>
 
 
-            {/* <span className='font-thin text-xs text-gray-800 hover:drop-shadow-[0_10px_30px_rgba(255,94,0,0.25)]'>{src.slice(0, 1).toUpperCase() + src.slice(1).toLowerCase()}</span> */}
+
         </a>
     )
 }
